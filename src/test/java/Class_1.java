21ec08389cc51d92ff2d1e7f3799cc4167fcedda
bhavnaacharya1986 <153376812+bhavnaacharya1986@users.noreply.github.com>
2023-12-20 15:59:41 +0000

    code added and updated

--- src/test/java/Class_1.java
+++ src/test/java/Class_1.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Class_1 {
+public class Class_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        System.out.println("I am a Java program.");
+    }
 }
