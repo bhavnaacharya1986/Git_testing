ff0ba84f0db37c81e5331a600447e6a340384805
bhavnaacharya1986 <153376812+bhavnaacharya1986@users.noreply.github.com>
2023-12-20 16:13:55 +0000

    Update Class_1.java
    code updated

--- src/test/java/Class_1.java
+++ src/test/java/Class_1.java
@@ -2,5 +2,6 @@ public class Class_1 {
     public static void main(String[] args) {
 
         System.out.println("This is my coding");
+        System.out.println("This is a git testing");
     }
 }
